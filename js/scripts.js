3144b7cd303fc14ce89acc9fc0f716d4a7d540d9
Esteban <esteban29712@gmail.com>
2021-12-30 10:49:09 -0500

    added JS timer

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,25 @@ hamburgerBtn.addEventListener('click', function () {
 });
 
 
+var x = setInterval(function() {
+    var countDate = new Date("April 23, 2022 15:37:25").getTime();
+    var now = new Date().getTime();
+    var gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) /  hour);
+    const textMinute = Math.floor((gap % hour) /  minute);
+    const textSecond= Math.floor((gap % minute) /  second);
+
+    document.querySelector('.day').innerText = textDay
+    document.querySelector('.hour').innerText = textHour
+    document.querySelector('.minute').innerText = textMinute
+    document.querySelector('.second').innerText = textSecond
+
+    console.log(now, countDate)
+}, 1000);
