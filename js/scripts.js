6788a302d0ac5e7e891d5c0ab9471b10df72714b
Esteban <esteban29712@gmail.com>
2022-03-02 15:15:19 -0500

    js

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,6 @@ $('#switch-lang-mobile').click(function () {
     $('[lang="es"]').toggle();
     $('[lang="en"]').toggle();
     console.log("working")
-
-    if ($(this).attr('[lang="es"]')){
-        localStorage.setItem("span", JSON.stringify('[lang="es"]'))
-        localStorage.remove('[lang="en"]')
-    } else if ($(this).attr('[lang="en"]')){
-        localStorage.setItem("eng", JSON.stringify('[lang="en"]'))
-        localStorage.remove('[lang="es"]')
-    }
 });
 
 // Toggle mobile gradient so that it doesnt clash with the transparency of the menu when its open
@@ -47,27 +39,35 @@ $('.navbar-toggler').click(function(){
 
 // Index timer 
 var x = setInterval(function() {
-    if (!$('#body-home').length) return;
-    var countDate = new Date("April 23, 2022 12:00:00").getTime();
-    var now = new Date().getTime();
-    var gap = countDate - now;
+    if(window.location.pathname == '/index.html') {
+        if (!$('#body-home').length) return;
+        var countDate = new Date("April 23, 2022 12:00:00").getTime();
+        var now = new Date().getTime();
+        var gap = countDate - now;
 
-    var second = 1000;
-    var minute = second * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) /  hour);
-    const textMinute = Math.floor((gap % hour) /  minute);
-    const textSecond= Math.floor((gap % minute) /  second);
+        var second = 1000;
+        var minute = second * 60;
+        var hour = minute * 60;
+        var day = hour * 24;
+        
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) /  hour);
+        const textMinute = Math.floor((gap % hour) /  minute);
+        const textSecond= Math.floor((gap % minute) /  second);
 
-    document.querySelector('.day').innerText = textDay
-    document.querySelector('.hour').innerText = textHour
-    document.querySelector('.minute').innerText = textMinute
-    document.querySelector('.second').innerText = textSecond
-    
+        document.querySelector('.day').innerText = textDay
+        document.querySelector('.hour').innerText = textHour
+        document.querySelector('.minute').innerText = textMinute
+        document.querySelector('.second').innerText = textSecond
+    }
 }, 1000);
 // Event is at 12pm Central, so the "hour", "min", & "sec" values will read as "0" at 12pm every day, and one "day" will be subtracted
 
 
+//     if ($(this).attr('[lang="es"]')){
+    //     localStorage.setItem("span", JSON.stringify('[lang="es"]'))
+    //     localStorage.remove('[lang="en"]')
+    // } else if ($(this).attr('[lang="en"]')){
+    //     localStorage.setItem("eng", JSON.stringify('[lang="en"]'))
+    //     localStorage.remove('[lang="es"]')
+    // }
